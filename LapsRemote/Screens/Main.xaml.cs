163f22f31f2ecf4e34c0d80fa7c64383324594a1
Timothy Ganoza <jostimian@gmail.com>
2021-06-07 20:58:44 +0800

    Use MahApp For UI

--- LapsRemote/Screens/Main.xaml.cs
+++ LapsRemote/Screens/Main.xaml.cs
@@ -10,7 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using MahApps.Metro.Controls;
 using System.Windows.Shapes;
+using System.Diagnostics;
 using LapsRemote.Logging;
 
 namespace LapsRemote.Screens
@@ -18,11 +20,29 @@ namespace LapsRemote.Screens
     /// <summary>
     /// Interaction logic for Main.xaml
     /// </summary>
-    public partial class Main : Window
+    public partial class Main : MetroWindow
     {
         public Main()
         {
             InitializeComponent();
         }
-    }
+
+		private void OpenRepo(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				ProcessStartInfo psi = new ProcessStartInfo
+				{
+					UseShellExecute = true,
+					FileName = "https://github.com/jostimian/LapsRemoteV2"
+				};
+				Process.Start(psi);
+			}
+			catch (Exception exp)
+			{
+				Logger.MessageBoxLog("Cant open website", Level.Error, DateTime.Now);
+				Logger.Log(exp.StackTrace, Level.Error, DateTime.Now);
+			}
+		}
+	}
 }
