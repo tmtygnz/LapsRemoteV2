4fc76baf32cae5514e2c209e399de8b86f3fcf6b
Timothy Ganoza <jostimian@gmail.com>
2021-06-25 21:40:00 +0800

    InnoSetup File Added

--- LapsRemote/ViewsModel/MainViewModel.cs
+++ LapsRemote/ViewsModel/MainViewModel.cs
@@ -11,6 +11,11 @@ using LapsRemote.Utilities;
 using LapsRemote.Vitals;
 using System.Threading;
 using System.Windows;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Configurations;
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace LapsRemote.ViewsModel
 {
@@ -19,7 +24,23 @@ namespace LapsRemote.ViewsModel
 
 		public MainViewModel()
 		{
+			Model = new PlotModel { Title = "Hello World" };
 			new Thread(new ThreadStart(UpdateVitals)).Start();
+		}
+
+
+		private PlotModel model;
+		public PlotModel Model
+		{
+			get => model;
+
+			set
+			{
+				if (value == model)
+					return;
+				model = value;
+				OnPropertyChanged();
+			}
 		}
 
 		public ICommand OpenRepositoryWebsite_Command => new RelayCommand(param => OpenRepositoryWebsite_Action());
@@ -129,11 +150,12 @@ namespace LapsRemote.ViewsModel
 			{
 				while (true)
 				{
-					Thread.Sleep(100);
+					Thread.Sleep(1000);
 					TemperatureString = Temperature.RandomTemperature().ToString();
 					OxyStatString = OxyStat.RandomOxyStat().ToString();
 					BPMString = BPM.RandomBPM().ToString();
 					RespRateString = RespRate.RandomRespRate().ToString();
+					Model.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
 				}
 			}
 		}
